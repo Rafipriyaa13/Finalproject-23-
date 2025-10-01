e9035e9bfd2a90c6182b9b34bb5e48605f4b6b33
Resya Fajar Putra P <112920059+SakamaRey@users.noreply.github.com>
2025-10-01 13:52:29 +0700

    Modifikasi baris pertama oleh Resya

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 public class Test{
     public static void main (String[]args){
-        System.out.println("HI,Ini baris kode pertama");
+        System.out.println("HI, Ini baris kode pertama dimodifikasi oleh Resya");
     }
 }
